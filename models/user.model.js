473fa3d92a4a2eb2565746cf234f6b2544b21588
sebasGZA <sebassga34@gmail.com>
2023-03-13 18:50:53 -0500

    Create usermodel using mongoose

---
+++ models/user.model.js
@@ -0,0 +1,32 @@
+const { Schema, model } = require("mongoose");
+
+const UserSchema = Schema({
+  name: {
+    type: String,
+    required: [true, "Name is required"],
+  },
+  email: {
+    type: String,
+    required: [true, "Email is required"],
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: [true, "Password is required"],
+  },
+  role: {
+    type: String,
+    required: true,
+    enum: ["ADMIN_ROLE", "USER_ROLE"],
+  },
+  state: {
+    type: Boolean,
+    default: true,
+  },
+  google: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+module.exports = model("User", UserSchema);
