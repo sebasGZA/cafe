668357be105607e9bee04fb49c9fe5f0b5f98e56
sebasGZA <sebassga34@gmail.com>
2023-03-13 19:55:49 -0500

    Custom validator for role using express-validator

--- routes/user.route.js
+++ routes/user.route.js
@@ -8,7 +8,7 @@ const {
   deleteUser,
 } = require("../controllers/user.controller");
 const { fieldsValidator } = require("../middlewares/fieldsValidator");
-
+const Role = require("../models/role.model");
 const router = Router();
 
 router.get("/", getUsers);
@@ -19,7 +19,14 @@ router.post(
     check("name", "Name is required").not().isEmpty(),
     check("email", "Email is not valid").isEmail(),
     check("password", "Password must have 6 letters").not().isEmpty(),
-    check("role", "Role is required").isIn(["ADMIN_ROLE", "USER_ROLE"]),
+    // check("role", "Role is required").isIn(["ADMIN_ROLE", "USER_ROLE"]),
+    check("role", "Role is required").custom(async (role = "") => {
+      const roleDb = await Role.findOne({ role });
+      if (!roleDb) {
+        throw new Error("Role is not valid");
+      }
+    }),
+
     fieldsValidator,
   ],
   postUser
